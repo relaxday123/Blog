bad2dc4f2693330a2bba88c3108065b6a62ec6ba
relaxday123 <91047548+relaxday123@users.noreply.github.com>
2023-02-23 14:00:34 +0700

    Updates

---
+++ precache-manifest.856ba63509d7636e5d35d1195ecd2f6a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c49aad8c220283b1e782",
+    "url": "/Blog/static/css/main.2b7fa463.chunk.css"
+  },
+  {
+    "revision": "c49aad8c220283b1e782",
+    "url": "/Blog/static/js/main.c49aad8c.chunk.js"
+  },
+  {
+    "revision": "5792cef5848f94440151",
+    "url": "/Blog/static/js/1.5792cef5.chunk.js"
+  },
+  {
+    "revision": "90618c8717a6cd422697",
+    "url": "/Blog/static/css/2.756b676b.chunk.css"
+  },
+  {
+    "revision": "90618c8717a6cd422697",
+    "url": "/Blog/static/js/2.90618c87.chunk.js"
+  },
+  {
+    "revision": "0e210599bc744ebb4eb7",
+    "url": "/Blog/static/js/runtime~main.0e210599.js"
+  },
+  {
+    "revision": "1f9ada00791f3dccac6a46c1bf1c9dd6",
+    "url": "/Blog/static/media/notFound.1f9ada00.jfif"
+  },
+  {
+    "revision": "ce67265f71918d82d3061b72800346ee",
+    "url": "/Blog/static/media/logo.fabd36fb.png"
+  },
+  {
+    "revision": "31a1478c5be38adab890d9aaa489c6dc",
+    "url": "/Blog/static/media/1.dee2e465.jpg"
+  },
+  {
+    "revision": "2a416336eb9ba2f298b9c79c65a4f813",
+    "url": "/Blog/static/media/5.3af1b315.jpg"
+  },
+  {
+    "revision": "f3113929b7742c5f57b587f48afac23d",
+    "url": "/Blog/static/media/2.bfe1f688.jpg"
+  },
+  {
+    "revision": "1508628c9f5b201f541212064a450c56",
+    "url": "/Blog/static/media/200w.2c851952.gif"
+  },
+  {
+    "revision": "59753e7736aa244bc0e4156fbe75790e",
+    "url": "/Blog/static/media/3.1cc1caf4.png"
+  },
+  {
+    "revision": "acefd720cfe9f47f70cb3b7e491a0ad1",
+    "url": "/Blog/static/media/4.79af6aac.jpg"
+  },
+  {
+    "revision": "b73d238419f66995de5861dda38c8781",
+    "url": "/Blog/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deploy-github/precache-manifest.77235ccdb91e5bfbcf360c84a04b7666.js"
+  "/Blog/precache-manifest.856ba63509d7636e5d35d1195ecd2f6a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/deploy-github/index.html", {
+workbox.routing.registerNavigationRoute("/Blog/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
